bb517852e954efab11e6d460ea4b4a6ecf409ec1
Superadofm <superradioadofm@gmail.com>
2025-02-02 16:40:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Démarrer Icecast
-icecast -c /etc/icecast.xml &
+# Lancer Icecast en arrière-plan
+/usr/bin/icecast2 -c /etc/icecast.xml &
 
 # Attendre quelques secondes pour s'assurer qu'Icecast est bien lancé
 sleep 5
 
-# Démarrer AutoDJ
-liquidsoap /usr/local/bin/liquidsoap_script.liq
+# Lancer Liquidsoap
+/usr/bin/liquidsoap /usr/local/bin/liquidsoap_script.liq
